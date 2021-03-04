5719414ab4765cc657311263305a74bd21673884
mightyfrog <mightyfrog.gc@gmail.com>
2021-03-05 04:28:30 +0900

    add workaround to android tracker issue 111907042

--- app/src/main/java/org/mightyfrog/android/preferencefragmentcompatsample/SettingsFragment.kt
+++ app/src/main/java/org/mightyfrog/android/preferencefragmentcompatsample/SettingsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceScreen
 import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -24,5 +25,25 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.prefs, rootKey)
+    }
+
+    /**
+     * https://issuetracker.google.com/issues/111907042
+     */
+    override fun setPreferenceScreen(preferenceScreen: PreferenceScreen?) {
+        super.setPreferenceScreen(preferenceScreen)
+
+        if (preferenceScreen != null) {
+            for (i in 0 until preferenceScreen.preferenceCount) {
+                preferenceScreen.getPreference(i).isIconSpaceReserved = false
+
+                // need to do the same to PreferenceCategory if there is any
+//                val pc = preferenceScreen.getPreference(i) as PreferenceCategory
+//                for (j in 0 until pc.preferenceCount) {
+//                    pc.getPreference(j).isIconSpaceReserved = false
+//                }
+//                pc.isIconSpaceReserved = false
+            }
+        }
     }
 }
